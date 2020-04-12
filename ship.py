969b5c2fbc6871b31788f2fd1a3b2e6346ffe0bc
wiselyku <wiselyku@gmail.com>
2020-04-12 21:41:17 +0800

    modife the ship.bmp to ship.png

--- ship.py
+++ ship.py
@@ -10,7 +10,7 @@ class Ship:
 
 
 		# Load the ship image and get its rect.
-		self.image = pygame.image.load('images/ship.bmp')
+		self.image = pygame.image.load('images/ship.png')
 		self.rect = self.image.get_rect()
 		# Start each new ship at the bottom center of the screen.
 		self.rect.midbottom = self.screen_rect.midbottom
